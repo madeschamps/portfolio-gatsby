7a4edf9efb4fea03fa9402dda37bbe36b17b868c
madeschamps <marc-andre.deschamps@logient.com>
2019-06-05 23:09:54 +0100

    update project page

--- src/templates/project.js
+++ src/templates/project.js
@@ -9,15 +9,21 @@ class ProjectTemplate extends Component {
 
     return (
       <Layout>
-       <div className='hero container-fluid bgc-primary'>
-          <div className='row h-50 text-center justify-content-center align-items-center'>
-            <h1 dangerouslySetInnerHTML={{ __html: currentPage.title }} />
+       <div className=' container-fluid hero bgc-primary'>
+          <div className='row text-center justify-content-center align-items-center project-head'>
+            <h1 className='project-head__title' dangerouslySetInnerHTML={{ __html: currentPage.title }} />
             <div dangerouslySetInnerHTML={{ __html: currentPage.content }} />          
           </div>
-          <div className='row justify-content-center'>
-            <div className='col-md-6 text-center'>
+          <div className='row justify-content-center pb-5'>
+            <div className='col-md-12 text-center'>
               <p>Kevin's Website made with WordPress. More details and link to come soon..</p>
-              <img className='img-fluid' src="https://i.ibb.co/58FBj4N/kevinsiteimg1.png" />
+            </div>
+            <div className='col-md-4 text-center'>
+              <img className='img-fluid mb-4' src="https://i.gyazo.com/768efa3442c6886ab15f743609496be4.gif" alt="Kv Tattoo"/>
+            </div>
+            <div className='col-md-6 text-center'>
+              <img className='img-fluid' src="https://i.gyazo.com/17997bd47af0cc123440519e8fa0ca88.gif" alt="Kv Tattoo" />
+              <h2>2019</h2>
             </div>
           </div>
         </div>
